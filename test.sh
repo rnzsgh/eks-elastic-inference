c1e17f7b506b1e05c65f84afd7ae7e3db1b1e118
Ryan <ryannitz@amazon.com>
2019-05-03 15:30:28 -0400

    Initial

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+STACK_NAME=eks-0
+NODE_GROUP_NAME=compute
+
+ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+
+AZ_0=us-east-1a
+AZ_1=us-east-1b
+AZ_2=us-east-1c
+
+KEY_NAME=rnzdev
+
+# https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html
+AMI_ID=ami-0abcb9f9190e867ab # us-east-1
+
+# This is the role used to create the stack - needs a trust relationship
+# for CloudFormation and Lambda
+ADMIN_ROLE_ARN=arn:aws:iam::$ACCOUNT_ID:role/AdminForEks
+
+USERNAME=$(aws sts get-caller-identity --output text --query 'Arn' | awk -F'/' '{print $2}')
+
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --template-body file://stack.cfn.yml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --role-arn $ADMIN_ROLE_ARN \
+  --parameters \
+  ParameterKey=AvailabilityZone0,ParameterValue=$AZ_0 \
+  ParameterKey=AvailabilityZone1,ParameterValue=$AZ_1 \
+  ParameterKey=AvailabilityZone2,ParameterValue=$AZ_2 \
+  ParameterKey=AdminUser,ParameterValue=$USERNAME \
+  ParameterKey=AdminRoleArn,ParameterValue=$ADMIN_ROLE_ARN \
+  ParameterKey=KeyName,ParameterValue=$KEY_NAME \
+  ParameterKey=NodeImageId,ParameterValue=$AMI_ID \
+  ParameterKey=NodeGroupName,ParameterValue=$NODE_GROUP_NAME
+
